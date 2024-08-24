f7a22f8a8eb561ecc17e9eb564a7fc8002f5d8d9
Eric Tran <erictran1547@gmail.com>
2024-08-23 17:02:45 -0700

    Finished the footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; 2030 PetSoft. All rights reserved.
+      </small>
+    </footer>
+  );
 }

5e9908f0126e77ebfa5af4a344e3576e4693185f
Eric Tran <erictran1547@gmail.com>
2024-09-01 09:51:36 -0700

    removed sleep statements from the server actions file

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -16,8 +16,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // --- user actions ---
 export async function logIn(prevState: unknown, formData: unknown) {
-  await sleep(1000);
-
   // check if formData is a FormData type
   if (!(formData instanceof FormData)) {
     return {
@@ -51,8 +49,6 @@ export async function logOut() {
   await signOut({ redirectTo: "/" });
 }
 export async function signUp(prevState: unknown, formData: unknown) {
-  await sleep(1000);
-
   // check if formData is a FormData type
   if (!(formData instanceof FormData)) {
     return {
@@ -101,8 +97,6 @@ export async function signUp(prevState: unknown, formData: unknown) {
 
 // --- pet actions ---
 export async function addPet(pet: unknown) {
-  await sleep(1000);
-
   const session = await checkAuth();
 
   const validatedPet = petFormSchema.safeParse(pet);
@@ -134,8 +128,6 @@ export async function addPet(pet: unknown) {
 
 export async function editPet(petId: unknown, petData: unknown) {
   try {
-    await sleep(1000);
-
     // authentication check
     const session = await checkAuth();
 
@@ -179,8 +171,6 @@ export async function editPet(petId: unknown, petData: unknown) {
 }
 
 export async function deletePet(petId: unknown) {
-  await sleep(1000);
-
   // authentication check
   const session = await checkAuth();
 
